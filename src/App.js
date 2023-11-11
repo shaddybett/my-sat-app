5bd49ec956732550181fc410de6a2c7a1f405290
Shaddy <shadybett540@gmail.com>
2023-11-11 19:13:19 +0300

    imported useefect and state hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Home from "./Home";
 export default function App() {
   return (
     <div>
       <h1>hello</h1>
+      <Home/>
     </div>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import React,{useState,useEffect} from 'react'
+
+export default function Home() {
+const[data,setData] = useState([])
+
+
+  return (
+    <div>Home</div>
+  )
+}
