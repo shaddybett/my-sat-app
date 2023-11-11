d504773e07773c9fbc6a6a75e4bf94be9141d9a3
Shaddy <shadybett540@gmail.com>
2023-11-11 19:39:33 +0300

    found the error

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,8 +3,25 @@ import React,{useState,useEffect} from 'react'
 export default function Home() {
 const[data,setData] = useState([])
 
-
+useEffect(()=>{
+    fetch("https://newsapi.org/v2/everything?q=apple&from=2023-11-09&to=2023-11-09&sortBy=popularity&apiKey=20c90a308e904e2ab5a58bc7699de1b0")
+    .then((Response)=> Response.json())
+    .then((news)=> setData(news))
+    .catch((error)=> console.error("Something is off",error))
+},[])
   return (
-    <div>Home</div>
+    <div>
+        <h1>Welcome</h1>
+        {
+            data.articles ? data.articles.map((article,index)=>(
+                <div key={index.id}>
+                <p>Title:{article.title}</p>
+                <img src={article.urlToImage} alt={article.title}/>
+                </div>
+            )) : <p>Loading, please wait</p>
+
+        }
+    </div>
+
   )
 }
