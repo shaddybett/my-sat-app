4ea2b9d6ec02c017b2ef58e067678cc91c8382fa
Shaddy <shadybett540@gmail.com>
2023-11-12 09:21:16 +0300

    switched to bots

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
 const[data,setData] = useState([])
 
 useEffect(()=>{
-    fetch("https://newsapi.org/v2/everything?q=apple&from=2023-11-09&to=2023-11-09&sortBy=popularity&apiKey=20c90a308e904e2ab5a58bc7699de1b0")
+    fetch(" http://localhost:4000/bots")
     .then((Response)=> Response.json())
     .then((news)=> setData(news))
     .catch((error)=> console.error("Something is off",error))
@@ -13,10 +13,10 @@ useEffect(()=>{
     <div>
         <h1>Welcome</h1>
         {
-            data.articles ? data.articles.map((article,index)=>(
+            data ? data.map((article,index)=>(
                 <div key={index.id}>
-                <p>Title:{article.title}</p>
-                <img src={article.urlToImage} alt={article.title}/>
+                <p>Title:{article.name}</p>
+                <img src={article.avatar_url} alt={article.name}/>
                 </div>
             )) : <p>Loading, please wait</p>
 
